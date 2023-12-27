a1911b3bef8e158c7c87fa78e6b8434c22921566
Andrew Cooks <acooks@gmail.com>
2023-12-27 17:40:07 +1000

    cleanup net-test autorun
    Network testing usually requires more than one device, and in that case
    the test configuration parameters must be specified per VM - outside of
    rapido.conf.
    
    Adding the net-test parameters to rapido.conf is unacceptable to upstream
    author/maintainer, so rapido doesn't currently have much to do in autorun.
    
    Signed-off-by: Andrew Cooks <acooks@gmail.com>

--- autorun/net_test.sh
+++ autorun/net_test.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # SPDX-License-Identifier: (LGPL-2.1 OR LGPL-3.0)
 
-# The tn40xx-test directory and test scripts will be copied
-# by the cut script, from the tn40xx-driver source,
-# as defined in the rapido.conf file.
+# This rapido test is for running a network device driver test suite.
+#
+# The test script and configuration is defined outside of rapido.
+# Refer to:
+# [the rapido guide](docs/network_device_driver_guide.md) and
+# [test suite](https://github.com/acooks/net-driver-test)
 
 _vm_ar_env_check || exit 1
 _vm_ar_dyn_debug_enable
 
 ip link set lo up
-
-/driver-tests/start.sh ${NET_TEST_DEV_VM} ${NET_TEST_KMOD}
